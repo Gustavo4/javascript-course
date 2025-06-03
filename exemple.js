1fa4fff5152de49f3f878e8723def2b44557a8c5
Gustavo4 <gustavordt@outlook.com>
2025-06-03 17:19:12 +0200

    entraînement avec les tableaux

--- exemple.js
+++ exemple.js
@@ -6,11 +6,22 @@ monTableau.splice(2, 1); // Supprime l'élément à l'index 2 (trois)
 console.log(monTableau[0]); // Affiche 'zéro'
 console.log(monTableau[3]); // Affiche 'cinq 
 
+for (const nombre in monTableau) {
+    monTableau[nombre] = monTableau[nombre].toUpperCase(); // Met chaque élément en majuscules
+    console.log(nombre); // Affiche chaque index du tableau
+}
+
+for (const nombre of monTableau) {
+    console.log(nombre); // for of affiche chaque valeur du tableau
+}
+
 let monTableau2D = [
     ['mark', 'john', 'jane'],
     ['alice', 'bob', 'charlie']
 ];
 
+monTableau2D.splice(2, 0, ['2', '22', '33']); // Ajoute un nouveau tableau à l'index 2
+console.log(monTableau2D[2]); // Affiche '2'
 console.log(monTableau2D[0][1]); // Affiche 'john'
 
 let monTableauAssociatif = {
