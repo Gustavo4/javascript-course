3a9a771ad39b57dd98f7c2b4056adb6a6529b4d8
Gustavo4 <gustavordt@outlook.com>
2025-07-04 17:34:49 +0200

    fetch get et fetch post

---
+++ exemple_fetch_recuperer_prix_bitcoin.js
@@ -0,0 +1,36 @@
+const url = 'https://blockchain.info/ticker';
+
+// function recupererPrix() {
+//   // Créer une requête HTTP GET
+//   let requete = new XMLHttpRequest();
+//   requete.open('GET', url);
+//   requete.responseType = 'json';
+//   requete.send();
+  
+//   // Dèss qu'on reçoit une réponse, cette fonction est appelée
+//   requete.onload = function () { 
+//     if (requete.readyState === XMLHttpRequest.DONE) {
+//       if (requete.status === 200) {
+//         let reponse = requete.response;
+//         let prixEnEuro = reponse.EUR.last;
+//         document.querySelector('#price_label').textContent = prixEnEuro;
+//       } else {
+//         alert('Un problème est intervenu, merci de revinir plus tard.');
+//       }
+//     }
+//     console.log('Prix actualisé');
+//    }
+// }
+
+// setInterval(recupererPrix, 1000);
+
+const requete = await fetch(url, {
+  method: 'GET',
+});
+
+if(!requete.ok) {
+  alert('Un problème est survenu');
+} else {
+  let donnees = await requete.json();
+  document.querySelector('#price_label').textContent = donnees.EUR.last;
+}
